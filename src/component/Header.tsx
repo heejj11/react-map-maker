c4d36976100068d5b3421512bec91e4d095c38b5
heejj11 <heejj1111@gmail.com>
2023-10-27 02:57:00 +0900

    feat: add header

---
+++ src/component/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="title_box">
+        <h2 className="title">React를 사용해 네이버 지도를 호출하고 API를 호출해 마커 표시하기</h2>
+      </div>
+    </header>
+  )
+}
+
+export default Header
